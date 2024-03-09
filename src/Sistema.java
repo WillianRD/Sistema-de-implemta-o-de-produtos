eb449bb031df1ede5ba4a085dbea7875703494ca
WillRD <willianrodriguesdos1@gmail.com>
2024-03-09 16:03:24 -0300

    Metodos: setUser, setRegistro

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,29 +1,44 @@
 import java.util.Scanner;
 
 class Sistema {
+    @SuppressWarnings("unused")
+    private static String login, nome_completo, password, cpf, nascimento;
     public static void main(String[] args) {
+        setUser();
+        setRegistro();
+    }
 
-        String login ="";
-        String password ="";
-        String nome_completo ="";
-        String data_de_nascimento = "";
-        String cpf ="";
-        var verificacao ="";
+    static void setUser() {
+        @SuppressWarnings("resource")
+        Scanner in = new Scanner(System.in);
 
-        Scanner verificacao = new Scanner(System.in);
-        System.out.println("Digite seu nome completo: ");
-        nome_completo = verificacao.nextLine();
+        System.out.println("Seu Login: ");
+        login = in.nextLine();
 
-        System.out.println("Digite sua data de nascimento: ");
-        System.out.println("Ex: xx/xx/xxxx");
-        data_de_nascimento =verificacao.nextLine();
 
-        System.out.println("Digite sem CPF com pontos e traços");
+        System.out.println("Sua senha de acesso: ");
+        System.out.println("Senha de 4 digitos: ");
+        password = in.nextLine();
+    }
+
+    static void setRegistro() {
+
+        @SuppressWarnings("resource")
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Nome completo: ");
+        nome_completo = in.nextLine();
+
+        System.out.println("Seu CPF: ");
         System.out.println("Ex: 123.456.789-00");
-        cpf = Verificacao.nextLine();
+        cpf = in.nextLine();
 
-        System.out.println("Verificação concluida com sucesso");
+        System.out.println("Data de nascimento: ");
+        System.out.println("Ex: xx/xx/xxxx ");
+        nascimento = in.nextLine();
+
+        System.out.println("Verifição concluida com sucesso");
         System.out.println("Aguarde para os proximos passos");
     }
-}
 
+}
